d845563f120d459dd1f200bf9189758bac4e2fa0
Xeniya Rogers <sana@xeniyas-mbp.lan>
2020-12-07 16:25:27 -0500

    Updating

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,24 @@
-print ("homework")
+# Import two modules necessary for this homework
 import os
-import csv
-csvbudget = os.path.join('..','Resources','budget_data.csv')
 
-with open(csvbudget) as csvfile:
-    csvreader = csv.reader(csvbudget, delimiter=',')
-    print(csvreader)
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+my_file = os.path.join(THIS_FOLDER, 'budget_data.csv')
+
+import csv
+
+with open(my_file) as budget:
+    reader = csv.reader(budget, delimiter=',')
+    
+    print(reader)
+
+    budget_header = next(reader) 
+    # print(f"Budget Header: {budget_header}")
+
+    for row in reader:
+        print(row)
+
+
+
 
 
 
